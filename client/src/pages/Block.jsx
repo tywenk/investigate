a2ac735d6a88d8c4265f5d3fdd612e96d3684333
Tywen Kelly <tywen.kelly@gmail.com>
2022-05-03 10:00:41 -0700

    adds tiptap

--- client/src/pages/Block.jsx
+++ client/src/pages/Block.jsx
@@ -1,6 +1,19 @@
 import StartInputForm from "../components/StartInputForm"
+import InstallMetaMaskButton from "../components/InstallMetaMaskButton"
+import { useUser } from "../context/UserContext"
 
 const Block = () => {
+	const currentUser = useUser()
+
+	if (!currentUser?.address) {
+		return (
+			<div>
+				Please Install metamsk to proceed
+				<InstallMetaMaskButton></InstallMetaMaskButton>
+			</div>
+		)
+	}
+
 	return (
 		<div>
 			<StartInputForm endpoint={"blocks"} route='block' />
